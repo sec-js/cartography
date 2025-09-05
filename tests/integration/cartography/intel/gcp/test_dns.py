c784aa30de0ce5642d5265037eac4138da5d81ee
Alex Chantavy <alex@subimage.io>
2025-09-05 10:49:52 -0700

    test: Add integration test for GCP DNS sync (#1880)
    GCP DNS test coverage. Prepping for a refactor.

--- tests/integration/cartography/intel/gcp/test_dns.py
+++ tests/integration/cartography/intel/gcp/test_dns.py
@@ -1,5 +1,10 @@
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
 import cartography.intel.gcp.dns
 import tests.data.gcp.dns
+from tests.integration.util import check_nodes
+from tests.integration.util import check_rels
 
 TEST_PROJECT_NUMBER = "000000000000"
 TEST_UPDATE_TAG = 123456789
@@ -131,3 +136,80 @@ def test_rrs_relationships(neo4j_session):
     actual = {(r["n1.id"], r["n2.id"]) for r in result}
 
     assert actual == expected
+
+
+@patch.object(
+    cartography.intel.gcp.dns,
+    "get_dns_rrs",
+    return_value=tests.data.gcp.dns.DNS_RRS,
+)
+@patch.object(
+    cartography.intel.gcp.dns,
+    "get_dns_zones",
+    return_value=tests.data.gcp.dns.DNS_ZONES,
+)
+def test_sync_dns_records(mock_get_zones, mock_get_rrs, neo4j_session):
+    """sync() loads DNS zones, record sets, and creates relationships."""
+    common_job_parameters = {"UPDATE_TAG": TEST_UPDATE_TAG}
+
+    # Ensure test GCPProject exists
+    neo4j_session.run(
+        """
+        MERGE (p:GCPProject{id: $PROJECT_NUMBER})
+        ON CREATE SET p.firstseen = timestamp()
+        SET p.lastupdated = $UPDATE_TAG
+        """,
+        PROJECT_NUMBER=TEST_PROJECT_NUMBER,
+        UPDATE_TAG=TEST_UPDATE_TAG,
+    )
+
+    cartography.intel.gcp.dns.sync(
+        neo4j_session,
+        MagicMock(),
+        TEST_PROJECT_NUMBER,
+        TEST_UPDATE_TAG,
+        common_job_parameters,
+    )
+
+    assert check_nodes(
+        neo4j_session,
+        "GCPDNSZone",
+        ["id"],
+    ) == {
+        ("111111111111111111111",),
+        ("2222222222222222222",),
+    }
+    assert check_nodes(
+        neo4j_session,
+        "GCPRecordSet",
+        ["id"],
+    ) == {
+        ("a.zone-1.example.com.",),
+        ("b.zone-1.example.com.",),
+        ("a.zone-2.example.com.",),
+    }
+    assert check_rels(
+        neo4j_session,
+        "GCPProject",
+        "id",
+        "GCPDNSZone",
+        "id",
+        "RESOURCE",
+        rel_direction_right=True,
+    ) == {
+        (TEST_PROJECT_NUMBER, "111111111111111111111"),
+        (TEST_PROJECT_NUMBER, "2222222222222222222"),
+    }
+    assert check_rels(
+        neo4j_session,
+        "GCPDNSZone",
+        "id",
+        "GCPRecordSet",
+        "id",
+        "HAS_RECORD",
+        rel_direction_right=True,
+    ) == {
+        ("111111111111111111111", "a.zone-1.example.com."),
+        ("111111111111111111111", "b.zone-1.example.com."),
+        ("2222222222222222222", "a.zone-2.example.com."),
+    }
